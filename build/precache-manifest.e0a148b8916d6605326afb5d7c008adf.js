a3e94329d2e26507bfabf32ad64f32148e79ece3
Abkeyev Daniyar <abkeyev@gmail.com>
2020-09-01 10:51:24 +0600

    publish

---
+++ build/precache-manifest.e0a148b8916d6605326afb5d7c008adf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a10febb51a3e6fc20e2213f7bb8a28",
+    "url": "/deferral/index.html"
+  },
+  {
+    "revision": "f63f31fd508becaecb67",
+    "url": "/deferral/static/css/main.f57910f7.chunk.css"
+  },
+  {
+    "revision": "a4fb6e0af16065c99bea",
+    "url": "/deferral/static/js/2.df86a64f.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/deferral/static/js/2.df86a64f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63f31fd508becaecb67",
+    "url": "/deferral/static/js/main.9cc50c22.chunk.js"
+  },
+  {
+    "revision": "d359c9707d08b96a241d",
+    "url": "/deferral/static/js/runtime-main.329a55dd.js"
+  }
+]);
